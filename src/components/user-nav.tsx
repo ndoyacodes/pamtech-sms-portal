75e794170a957940cd4c7045dd7101428faed692
widameDeograss <widambedeograss5@gmail.com>
2025-01-09 00:39:57 +0300

    feat: integrate dashboard data fetching in UserNav component and enhance SMS balance display with loading state

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -11,13 +11,14 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { useNavigate } from 'react-router-dom'
 import { useAuthStore } from '@/hooks/use-auth-store'
+import { useQuery } from '@tanstack/react-query'
+import { dashboardService } from '@/api/services/dashboard/dashboard.service'
 
 
-const formatCurrency = (amount: number) => {
+const formatNumber = (amount: number) => {
   return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'TZS',
-    minimumFractionDigits: 2
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0
   }).format(amount);
 };
 
@@ -25,6 +26,16 @@ const formatCurrency = (amount: number) => {
 export function UserNav() {
   const navigate = useNavigate()
   const {user} =  useAuthStore();
+
+  const { data: dashData, isLoading } = useQuery({
+    queryKey: ['dashboard',],
+    queryFn: async () => {
+      const response: any = await dashboardService.getCustomerDashboardData()
+      return response
+    },
+    retry: 2,
+    staleTime: 5 * 60 * 1000,
+  });
 
   // const [jwtToken,setJwtToken] =useAuthentication();
   function logOut() {
@@ -37,13 +48,19 @@ export function UserNav() {
   return (
     <div className='flex  items-center justify-center gap-2'>
       {user?.customer && (
-        <div className='flex flex-row gap-2 justify-start'>
-          <span className='font-bold flex flex-col'>
-            <span className='text-xs'>Balance</span>
-            <span className='text-sm'>
-          {formatCurrency(40000)}
-            </span>
-          </span>
+        <div className='flex flex-row gap-2 justify-start mr-2'>
+        {
+          isLoading ? (
+            <div className='flex h-12 items-center justify-center'>
+              <div className='h-3 w-3 animate-spin rounded-full border-b-2 border-gray-900 dark:border-white'></div>
+            </div>
+          ) : (
+            <div className='flex flex-col items-start justify-start'>
+              <p className='text-xs font-semibold text-muted-foreground'>SMS Balance</p>
+              <p className='text-sm font-semibold'>{formatNumber(dashData?.smsBalance)} SMS</p>
+            </div>
+          )
+        }
 
           <Button
             className=''
@@ -57,9 +74,9 @@ export function UserNav() {
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
-            <Avatar className='h-8 w-8'>
+            <Avatar className='h-10 w-10'>
               <AvatarImage src='/avatars/01.png' alt='@shadcn' />
-              <AvatarFallback>
+              <AvatarFallback >
                 {user?.firstName?.charAt(0).toUpperCase()}  {user?.lastName?.charAt(0).toUpperCase()}
               </AvatarFallback>
             </Avatar>
