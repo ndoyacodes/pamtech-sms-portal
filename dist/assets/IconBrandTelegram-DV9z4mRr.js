4783f89b38e56328ee79b597807f9317b9181ffb
nndoya <yarndoya@outlook.com>
2025-04-14 20:01:06 +0300

    Fixes

--- dist/assets/IconBrandTelegram-DV9z4mRr.js
+++
@@ -1,7 +0,0 @@
-import{b as l}from"./index-DgsANLC3.js";
-/**
- * @license @tabler/icons-react v3.31.0 - MIT
- *
- * This source code is licensed under the MIT license.
- * See the LICENSE file in the root directory of this source tree.
- */var a=l("outline","brand-telegram","IconBrandTelegram",[["path",{d:"M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4",key:"svg-0"}]]);export{a as I};
