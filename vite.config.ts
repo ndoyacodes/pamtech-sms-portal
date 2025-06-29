9950f9145665a3b3013bf2f24f21161dfc130470
A266726 <ndoya.yared@stanbic.co.tz>
2025-06-29 15:22:46 +0300

    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import rollupReplace from "@rollup/plugin-replace";
 import obfuscatorPlugin from 'rollup-plugin-obfuscator';
+import viteImagemin from 'vite-plugin-imagemin';
+import imageminWebp from 'imagemin-webp';
+
 
 export default defineConfig(({ mode }) => {
   loadEnv(mode, process.cwd(), '')
@@ -48,7 +51,38 @@ export default defineConfig(({ mode }) => {
           stringArray: true,
           stringArrayThreshold: 0.75
         }
-      })
+      }),
+      viteImagemin({
+        gifsicle: {
+          optimizationLevel: 3,
+          interlaced: false,
+        },
+        optipng: {
+          optimizationLevel: 7, // For PNG
+        },
+        mozjpeg: {
+          quality: 75, // For JPG, JPEG
+        },
+        pngquant: {
+          quality: [0.65, 0.9], // For PNG
+          speed: 4,
+        },
+        svgo: {
+          plugins: [
+            { name: 'removeViewBox', active: false },
+            { name: 'removeEmptyAttrs', active: false },
+          ],
+        },
+
+        // WebP compression support
+        plugins: [
+          imageminWebp({
+            quality: 75,
+          }),
+        ],
+        filter: (source) =>
+          /\.(png|jpe?g|webp)$/i.test(source),
+      }),
     ].filter(Boolean),
     build: {
       minify: 'terser',
